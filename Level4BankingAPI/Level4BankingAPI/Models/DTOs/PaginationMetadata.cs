53a27bcdb1c364c09f5f94c1c0f45f1eead12225
ComradeRyn <ryanyuncza@gmail.com>
2026-02-05 10:03:38 -0500

    Created PaginationMetadata, GetAccountsRequest, TokenResponse records

---
+++ Level4BankingAPI/Level4BankingAPI/Models/DTOs/PaginationMetadata.cs
@@ -0,0 +1,6 @@
+﻿namespace Level4BankingAPI.Models.DTOs;
+
+public record PaginationMetadata(int TotalItemCount,
+    int TotalPageCount,
+    int PageSize,
+    int CurrentPage);
---
+++ Level4BankingAPI/Level4BankingAPI/Models/DTOs/Requests/GetAccountsRequest.cs
@@ -0,0 +1,8 @@
+﻿namespace Level4BankingAPI.Models.DTOs.Requests;
+
+public record GetAccountsRequest(
+    string? Name,
+    string? SortType,
+    string? SortOrder,
+    int PageNumber,
+    int PageSize);
