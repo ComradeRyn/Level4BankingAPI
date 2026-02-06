cb557c42b2c630d37567c141dcd5ab6013ce1884
ComradeRyn <ryanyuncza@gmail.com>
2026-02-06 09:42:47 -0500

    Added swagger documentation to endpoints

--- Level4BankingAPI/Level4BankingAPI/Program.cs
+++ Level4BankingAPI/Level4BankingAPI/Program.cs
@@ -1,3 +1,4 @@
+using System.Reflection;
 using Microsoft.EntityFrameworkCore;
 using Level4BankingAPI.Clients;
 using Level4BankingAPI.Interfaces;
@@ -13,8 +14,17 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+builder.Services.AddEndpointsApiExplorer(); 
 builder.Services.AddOpenApi();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(setupAction =>
+{
+    var xmlCommentsFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+    var xmlCommentsFullPath = Path.Combine(AppContext.BaseDirectory, xmlCommentsFile);
+    
+    setupAction.IncludeXmlComments(xmlCommentsFullPath);
+});
+
+
 builder.Services.AddDbContext<AccountContext>(opt => opt.UseInMemoryDatabase("AccountList"));
 
 builder.Services.AddHttpClient<ICurrencyClient, CurrencyClient>(client =>
