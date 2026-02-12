5c9c757a8f234917d82cd2fa174965b4ed307de3
ComradeRyn <ryanyuncza@gmail.com>
2026-02-12 13:22:54 -0500

    Reformated AuthenticationServices

--- Level4BankingAPI/Level4BankingAPI/Services/AuthenticationService.cs
+++ Level4BankingAPI/Level4BankingAPI/Services/AuthenticationService.cs
@@ -17,7 +17,9 @@ public class AuthenticationService
     {
         var securityKey = new SymmetricSecurityKey(
             Convert.FromBase64String(_configuration["Authentication:SecretForKey"]!));
+        
         var signingCredentials = new SigningCredentials(securityKey, SecurityAlgorithms.HmacSha256);
+        
         var jwtSecurityToken = new JwtSecurityToken(
             _configuration["Authentication:Issuer"],
             _configuration["Authentication:Audience"],
@@ -25,6 +27,7 @@ public class AuthenticationService
             DateTime.UtcNow,
             DateTime.UtcNow.AddHours(1),
             signingCredentials);
+        
         var tokenToReturn = new JwtSecurityTokenHandler().WriteToken(jwtSecurityToken);
         
         return new TokenResponse(tokenToReturn);
